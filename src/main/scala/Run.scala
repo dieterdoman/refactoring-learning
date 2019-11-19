d78bba9b3c971a315bfbbaeef56062bd7f8e1cb1
DieterDoman <dieter.doman@gmail.com>
2019-11-19 12:06:42 +0200

    Minor cleanup.

--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 
 object Run {
   def main(args: Array[String]): Unit = {
-    val testTxtSource = scala.io.Source.fromFile("inputCheckSum.txt")
+    val testTxtSource = Source.fromFile("inputCheckSum.txt")
     val inputCheckSum = testTxtSource.getLines().mkString
     val checkSum = CheckSum.performCheckSum(inputCheckSum)
     println(checkSum)
--- src/test/scala/CheckSumTest.scala
+++ src/test/scala/CheckSumTest.scala
@@ -1,8 +1,9 @@
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import scala.io.Source
 
 class CheckSumTest extends FunSuite with BeforeAndAfterAll {
     test("CheckSum.performCheckSum") {
-      val testTxtSource = scala.io.Source.fromFile("inputCheckSumTest.txt")
+      val testTxtSource = Source.fromFile("inputCheckSumTest.txt")
       val inputCheckSum = testTxtSource.getLines().mkString
       val checkSum = CheckSum.performCheckSum(inputCheckSum)
       assert(checkSum === 12)
