55cdf3294512f279ff41f7d13cedb202c155472a
DieterDoman <dieter.doman@gmail.com>
2019-11-23 12:58:27 +0200

    concatDoubleTrippleString

--- src/main/scala/CheckSum.scala
+++ src/main/scala/CheckSum.scala
@@ -4,15 +4,9 @@ object CheckSum {
   def performCheckSum(boxIds: List[String]): Int = {
     val distinctCharInBoxIds = calculateDistinctCharacters(boxIds)
     val doubleTripleListPerBoxId = filterDoubleTriples(distinctCharInBoxIds)
-    var doubleString = ""
-    var tripleString = ""
-    doubleTripleListPerBoxId.foreach(_.foreach(charCount =>
-      if (charCount._2 == 2)
-        doubleString = doubleString.concat(charCount._1.toString)
-      else
-        tripleString = tripleString.concat(charCount._1.toString)
-      )
-    )
+    val dtTouple = concatDoubleTrippleString(doubleTripleListPerBoxId)
+    val doubleString = dtTouple._1
+    val tripleString = dtTouple._2
     val uniqueDoubleCount = doubleString.toSeq.distinct.unwrap.length
     val uniqueTripleCount = tripleString.toSeq.distinct.unwrap.length
     uniqueDoubleCount * uniqueTripleCount
@@ -29,4 +23,18 @@ object CheckSum {
   def filterDoubleTriples(distinctCharInBoxIds: List[List[(Char, Int)]]): List[List[(Char, Int)]] = {
     distinctCharInBoxIds.map(_.filter(charCount => charCount._2 == 2 || charCount._2 == 3))
   }
+
+  def concatDoubleTrippleString(doubleTripleListPerBoxId: List[List[(Char, Int)]]): (String, String) = {
+    var doubleString = ""
+    var tripleString = ""
+    doubleTripleListPerBoxId.foreach(_.foreach(charCount =>
+      if (charCount._2 == 2)
+        doubleString = doubleString.concat(charCount._1.toString)
+      else
+        tripleString = tripleString.concat(charCount._1.toString)
+    )
+    )
+    (doubleString, tripleString)
+  }
+
 }
