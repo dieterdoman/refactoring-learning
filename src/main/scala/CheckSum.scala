9b8c5a6ad85055b686f3c8ac99f3060ab86e3d3b
DieterDoman <dieter.doman@gmail.com>
2019-11-23 12:43:30 +0200

    Extracted calclulating distinct characters method

--- src/main/scala/CheckSum.scala
+++ src/main/scala/CheckSum.scala
@@ -2,11 +2,7 @@ object CheckSum {
   val END_LINE_CHAR = sys.props("line.separator")
 
   def performCheckSum(boxIds: List[String]): Int = {
-    val distinctCharInBoxIds = boxIds.map(boxId =>
-      boxId.toSeq.distinct.unwrap.toList.map(char =>
-        (char, boxId.count(_ == char))
-      )
-    )
+    val distinctCharInBoxIds = calculateDistinctCharacters(boxIds)
     val doubleTripleListPerBoxId = distinctCharInBoxIds.map(_.filter(charCount => charCount._2 == 2 || charCount._2 == 3))
     var doubleString = ""
     var tripleString = ""
@@ -20,5 +16,13 @@ object CheckSum {
     val uniqueDoubleCount = doubleString.toSeq.distinct.unwrap.length
     val uniqueTripleCount = tripleString.toSeq.distinct.unwrap.length
     uniqueDoubleCount * uniqueTripleCount
+  }
+
+  def calculateDistinctCharacters(boxIds: List[String]): List[List[(Char, Int)]] = {
+    boxIds.map(boxId =>
+      boxId.toSeq.distinct.unwrap.toList.map(char =>
+        (char, boxId.count(_ == char))
+      )
+    )
   }
 }
