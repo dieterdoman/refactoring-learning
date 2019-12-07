04af932f7e3863c313410c83cf874a0730c0e8d1
DieterDoman <dieter.doman@gmail.com>
2019-12-07 14:19:08 +0200

    GreenLight Challenge 2

--- src/main/scala/Overlap.scala
+++ src/main/scala/Overlap.scala
@@ -1,5 +1,32 @@
 object Overlap {
   def performOverlapCheck(lines: List[String]): Int = {
-    0
+    val spaceLines = lines.map(_.replace(" ", ""))
+    val claimInfos = spaceLines.map(element => element.split('@')(1))
+    val foobar = claimInfos.map(element => element.split(":"))
+    val claimBlocks = foobar.map(element => (element(0).split(","),  element(1).split("x")))
+    val size = 10
+    val MehMatrix = Array.ofDim[Int](size,size)
+    claimBlocks.foreach(element => {
+      val left = element._1(0).toInt
+      val top = element._1(1).toInt
+      val width = element._2(0).toInt
+      val height = element._2(1).toInt
+      for(col <- left until (left + width))
+      {
+        for(row <- top until (top + height))
+          {
+            MehMatrix(col)(row) = MehMatrix(col)(row) + 1
+          }
+      }
+    })
+    // matrix to int
+    var finalcountdown_dededewdo = 0
+    for(col <- 0 until size){
+      for(row <- 0 until size){
+        if(MehMatrix(col)(row) > 1)
+          finalcountdown_dededewdo = finalcountdown_dededewdo + 1
+      }
+    }
+    finalcountdown_dededewdo
   }
 }
